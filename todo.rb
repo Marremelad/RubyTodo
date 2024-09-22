2afec55650ebf2905a307ad2866d8b0ce7f9bbaf
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-09-22 13:33:55 +0200

    Change program structure, add comments

--- todo.rb
+++ todo.rb
@@ -1,30 +1,41 @@
+#Display tasks.
+def display_tasks(tasks)
+  if tasks.length == 0 #If there are no tasks, return.
+    return
+  end
 
+  puts "\nYou have the following tasks:"
+  tasks.each_with_index do |task, index| #Display the tasks.
+    puts "#{index + 1}. #{task[:name]} - #{task[:completed]}
+\nPress the tasks number followed by enter to change it's status"
+
+    chosen_task = gets.chomp.to_i
+    mark_task((chosen_task - 1), tasks)
+  end
+end
+
+#Add a task.
 def add_task(tasks)
   puts "Would you like to add a task? (yes/no)"
+
   user_input = gets.chomp
   user_input.downcase == "yes" ? name_task(tasks) : (puts "No tasks added.")
 end
 
+#Name the task.
 def name_task(tasks)
   puts "What task would you like to add?"
   task = {name: gets.chomp, completed: false}
   tasks << task
 end
 
-def display_tasks(tasks)
-  puts "\nYou have the following tasks:"
-  tasks.each_with_index do |task, index|
-    puts "#{index + 1}. #{task[:name]} - #{task[:completed]}
-\nPress the tasks number followed by enter to change it's status"
-    chosen_task = gets.chomp.to_i
-    mark_task((chosen_task - 1), tasks)
-    end
-end
-
+#Mark the task as complete or incomplete.
 def mark_task(index, tasks)
   puts "#{tasks[index][:name]} status: #{tasks[index][:completed]}
 \nWould you like to change the status of #{tasks[index][:name]}? (yes/no)"
+
   user_input = gets.chomp
+
   if user_input.downcase == "yes"
     if !tasks[index][:completed]
       tasks[index][:completed] = true
@@ -38,5 +49,5 @@ def mark_task(index, tasks)
 end
 
 task = []
-add_task(task)
 display_tasks(task)
+add_task(task)
